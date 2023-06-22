574be3da2ab3a4bd41da6e7343f4d16da7dccebd
Saurabh Tiwari <sbt5731@gmail.com>
2023-06-23 00:11:15 +0530

    basic react

---
+++ src/Components/Home.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import Task from './Task';
+
+
+
+
+const Home = () => {
+  const initialArray = localStorage.getItem("tasks")?JSON.parse(localStorage.getItem("tasks")) :
+  [];
+  const [tasks,setTasks] = useState(initialArray);
+  const [title, setTitle] = useState("");
+  const  [description,setDescription] = useState("");
+  console.log(...tasks)
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    setTasks([...tasks,{
+      title,
+      description
+    }])
+    setTitle("");
+    setDescription("");
+   };
+
+   
+
+  useEffect(()=>{
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  },[tasks])
+  const deleteTask =(index) => {
+  const filteredArr = tasks.filter((val,i)=>{
+    return i !== index;
+  })
+  console.log(filteredArr)
+  setTasks(filteredArr)
+  }
+  
+
+
+  return (
+    <div className='container'>
+        <h1>DAILY GOALS</h1>
+        <form onSubmit={submitHandler}> 
+          <input type='text' placeholder='Title'  
+          value={title}
+          onChange={(e) =>setTitle(e.target.value)}/>
+          <textarea placeholder='Description'
+          value={description}
+          onChange={(e) =>setDescription(e.target.value)}> 
+          </textarea>
+          <button type='submit'>ADD </button>
+        </form>
+
+        {tasks.map((item,index) => (
+          <Task key = {index}
+          title={item.title}
+          description={item.description} 
+          deleteTask={deleteTask}
+          index={index}
+          />
+        ))}
+
+
+    </div>
+  )
+};
+
+export default Home;
